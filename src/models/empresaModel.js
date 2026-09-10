b06ada1d5d8f1cc73ace7a5ec180260b00ea97bf
brxnogoncalves <bruno.goncalves@sptech.school>
2026-09-10 10:28:05 -0300

    fix: corrige gerador de código empresa para gerar códigos unicos

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,14 +1,9 @@
 var database = require("../database/config");
 
-function gerarCodigoEmpresa() {
-    return Math.random().toString(36).substring(2, 10).toUpperCase();
-}
-
-function cadastrar(razaoSocial, cnpj) {
-    var codigoEmpresa = gerarCodigoEmpresa();
+function cadastrar(razaoSocial, cnpj, codigoEmpresa) {
     var instrucaoSql = `
-        INSERT INTO empresa (razao_social, cnpj, codigo_empresa, criado_em, atualizado_em)
-        VALUES ('${razaoSocial}', '${cnpj}', '${codigoEmpresa}', NOW(), NOW());
+        INSERT INTO empresa (razao_social, cnpj, codigo_empresa)
+        VALUES ('${razaoSocial}', '${cnpj}', '${codigoEmpresa}');
     `;
     return database.executar(instrucaoSql);
 }
